5a573bd6094a3c0049e883a48e62e3110f8b4419
MariaaPcsa <marialujivan@gmail.com>
2025-04-29 07:04:04 -0300

    .

--- src/pages/Generosity.jsx
+++ src/pages/Generosity.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import '../index.css';
 import '../App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+
 
 const Generosity = () => {
   return (
